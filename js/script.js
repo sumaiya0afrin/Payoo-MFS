619228efe12de4aa15b9b7a687567c94e20dcc48
Sumaiya Afrin <sumaiya.afrin1008@gmail.com>
2024-09-19 14:35:33 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById('login_btn').addEventListener('click', (event)=>{
+        event.preventDefault();
+        
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+
+        if(phoneNumber === '0123456789' && pinNumber=== "123"){
+            console.log('you are logged in')
+            window.location.href = './home.html'
+        }else{
+            alert('Wrong Input')
+        }
+})
